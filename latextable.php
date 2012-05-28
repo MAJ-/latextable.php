66e1c385f7cd6006c14e6a6cd02bbe1aaf3d8cb4
MAJ <x0@lavabit.com>
2012-05-28 15:24:52 +0200

    cody style, and changed html declaration

--- latextable.php
+++ latextable.php
@@ -1,6 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 <title>Simple Latex-Table generator</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -85,15 +84,8 @@
 <h2>Features:</h2>
 
 <ul>
-<li>Anti-XSS measures</li>
-<li>&amp; replaced by \&amp; (LaTeX-compatible)</li>
+	<li>Anti-XSS measures</li>
+	<li>&amp; replaced by \&amp; (LaTeX-compatible)</li>
 </ul>
-<p>
-  <br />
-    <a href="http://validator.w3.org/check?uri=referer">
-    <img src="http://www.w3.org/Icons/valid-xhtml10"
-         alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
-  </p>
-
 </body>
 </html>
